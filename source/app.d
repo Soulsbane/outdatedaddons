bd3e8c6f978e432294dc641403e4dd64720d5990
Paul Crane <paul@acheronsoft.com>
2018-07-18 21:49:09 -0500

    Update CURRENT_INTERFACE_VERSION to 80000 for BfA.

--- source/app.d
+++ source/app.d
@@ -12,7 +12,7 @@ import luaaddon.tocparser;
 import ctoptions.structoptions;
 import ctoptions.getoptmixin;
 
-enum CURRENT_INTERFACE_VERSION = 70300;
+enum CURRENT_INTERFACE_VERSION = 80000;
 
 // Used to add additional methods to TocParser.
 struct AdditionalMethods
